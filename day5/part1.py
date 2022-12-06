e46fb32134cb6013c9e75139843636a5c634449a
John Nastase <john.nastase@gmail.com>
2022-12-05 21:19:26 -0500

    day 5 solutions

---
+++ day5/part1.py
@@ -0,0 +1,40 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+
+input = [x for x in lines]
+
+def create_crates(input):
+    defs = [x for x in input if "[" in x]
+    crates = []
+
+    for item in defs:
+        line = item
+        index = 0
+        while len(line):
+            val = line[:4]
+            line = line[4:]
+            crate = val[1].strip()
+            
+            if len(crates) <= index:
+                crates.append([])
+
+            if crate:
+                crates[index].insert(0, crate)
+
+            index += 1
+    return crates
+
+crates = create_crates(input)
+
+for line in [x for x in input if x.startswith("move")]:
+    vals = line.strip().split()
+    move =int(vals[1])
+    fro =int(vals[3])-1
+    to =int(vals[5])-1
+
+    for index in range(move):
+        item = crates[fro].pop()
+        crates[to].append(item)
+        
+
+print("".join([x[len(x)-1] for x in crates]))
